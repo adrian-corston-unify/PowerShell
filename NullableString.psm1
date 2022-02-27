f18b291c58021350e44d4fa9c278c939f83362f2
adrian-corston-unify <74707637+adrian-corston-unify@users.noreply.github.com>
2022-02-27 11:43:11 +1030

    For when you really want your [string] to be null

--- NullableString.psm1
+++ NullableString.psm1
@@ -4,17 +4,19 @@ class NullableString {
         $this.Value = $Value
     }
     [string] ToString() {
-        return [string]($this.Value)
+        return [string]$this.Value
     }
-    [string] ToUpper() {
-        return (([string]$this.Value).ToUpper())
+    [NullableString] ToUpper() {
+        if ($Null -eq $this.Value) { return [NullableString]::New($Null) }
+        return [NullableString]([string]$this.Value).ToUpper()
     }
-    [string] ToLower() {
-        return (([string]$this.Value).ToLower())
+    [NullableString] ToLower() {
+        if ($Null -eq $this.Value) { return [NullableString]::New($Null) }
+        return [NullableString]([string]$this.Value).ToLower()
     }
     static [NullableString[]] op_Addition([NullableString] $One, [NullableString] $Two) {
         if ($Null -eq $One) { return [NullableString]::New($Two.Value) }
         if ($Null -eq $Two) { return [NullableString]::New($One.Value) }
-        return [NullableString]::New($One.ToString() + $Two.ToString())
+        return [NullableString]::New([string]$One.Value + [string]$Two.Value)
     }
 }
